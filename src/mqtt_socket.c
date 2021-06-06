cb7047d572aba04af70fa0010ab4e889189de881
Yonggang Luo <luoyonggang@gmail.com>
2021-06-06 19:44:47 +0800

    Do not covert error code in mqtt_socket.c

--- src/mqtt_socket.c
+++ src/mqtt_socket.c
@@ -174,9 +174,6 @@ int MqttSocket_Write(MqttClient *client, const byte* buf, int buf_len,
             rc = MQTT_CODE_CONTINUE;
         }
     }
-    else if (rc == EWOULDBLOCK || rc == EAGAIN) {
-        rc = MQTT_CODE_CONTINUE;
-    }
 
 #else
     do {
@@ -259,10 +256,6 @@ int MqttSocket_Read(MqttClient *client, byte* buf, int buf_len, int timeout_ms)
             rc = MQTT_CODE_CONTINUE;
         }
     }
-    else if (rc == EWOULDBLOCK || rc == EAGAIN) {
-        rc = MQTT_CODE_CONTINUE;
-    }
-
 #else
     do {
         rc = MqttSocket_ReadDo(client, &buf[client->read.pos],
