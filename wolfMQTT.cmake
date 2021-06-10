489f8864b5d4fd86ae555c9402bb839c61895774
Yonggang Luo <luoyonggang@gmail.com>
2021-06-11 02:28:48 +0800

    Add initial cmake project with all examples.
    user settings update

---
+++ wolfMQTT.cmake
@@ -0,0 +1,32 @@
+
+function(wolfmqtt_steup_target target_name)
+  target_compile_definitions(${target_name} PRIVATE
+    BUILDING_WOLFMQTT
+  )
+  target_include_directories(${target_name} PUBLIC
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}
+  )
+  if (WIN32)
+    get_target_property(TARGET_TYPE ${target_name} TYPE)
+    if ("${TARGET_TYPE}" STREQUAL "SHARED_LIBRARY")
+      target_compile_definitions(${target_name} PUBLIC _WINDLL)
+    endif()
+  endif()
+
+  target_sources(${target_name} PRIVATE
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/mqtt_client.c
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/mqtt_packet.c
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/src/mqtt_socket.c
+  )
+endfunction(wolfmqtt_steup_target)
+
+function(wolfmqtt_steup_app_target target_name)
+  if (WIN32)
+    target_link_libraries(${target_name} ws2_32)
+  endif()
+  target_sources(${target_name} PRIVATE
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/examples/mqttexample.c
+    ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/examples/mqttnet.c
+  )
+  target_link_libraries(${target_name} wolfmqtt)
+endfunction(wolfmqtt_steup_app_target)
