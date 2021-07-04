69bc5bf5a537c74c3ebe9383de45411e9313e9ec
Yonggang Luo <luoyonggang@gmail.com>
2021-07-05 02:01:07 +0800

    Add wm_SemTryLock

--- src/mqtt_client.c
+++ src/mqtt_client.c
@@ -151,6 +151,12 @@ static int MqttClient_Publish_ReadPayload(MqttClient* client,
         WaitForSingleObject(*s, INFINITE);
         return 0;
     }
+    int wm_SemTryLock(wm_Sem* s) {
+        if (WaitForSingleObject(*s, 0) == WAIT_OBJECT_0) {
+            return 0;
+        }
+        return MQTT_CODE_CONTINUE;
+    }
     int wm_SemUnlock(wm_Sem *s) {
         ReleaseSemaphore(*s, 1, NULL);
         return 0;
@@ -789,7 +795,11 @@ static int MqttClient_SendObject(MqttClient* client, MqttSendObjectOption *optio
     #endif
     #ifdef WOLFMQTT_MULTITHREAD
         /* Lock send socket mutex */
+    #ifdef WOLFMQTT_NONBLOCK
+        rc = wm_SemTryLock(&client->lockSend);
+    #else
         rc = wm_SemLock(&client->lockSend);
+    #endif
         if (rc != 0) {
             return rc;
         }
